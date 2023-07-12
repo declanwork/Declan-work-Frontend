d901e6b547dc0caada338e42ec16b911238c56b6
Victor-Okpukpan <vokpukpan@gmail.com>
2023-07-12 16:12:05 +0100

    deleted files

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -72,7 +72,7 @@ export default function Navbar() {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <SearchBar />
                 <Link
-                  href="/signIn"
+                  href="/sign-in"
                   className="text-[#ADFFC8] text-sm p-1 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
                   Log In
