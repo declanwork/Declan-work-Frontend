7d587072868dd65869c0c4248ae5ef69f361bbc1
Nathaniyell <essien.nathan@yahoo.com>
2023-07-14 14:02:47 +0100

    Add proposals component

---
+++ src/app/Job/MyJobs/proposals/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/footer";
+
+export default function Proposals() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center p-4 mt-16 md:w-3/5 mx-auto text-center">
+
+
+        <h1>Hello World</h1>
+      </div>
+
+      <section className="py-20 bg-[#00360C]">
+        <Footer />
+      </section>
+    </>
+  );
+}
