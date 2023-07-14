da8cff1d26430fca98694d09c4133dccfd13bf0e
Nathaniyell <essien.nathan@yahoo.com>
2023-07-14 13:12:58 +0100

    Add Jobs component, update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors:{
+      lightGreen: "#00EF7C",
+      darkGreen: "#001e00"
+    },
     extend: {
       fontFamily:{
         poppin: ['Poppins', 'sans-serif'],
